4d51236deb5dfb44ce3ba865f34d7bab1a41480d
harleylizard <64570529+harleylizard@users.noreply.github.com>
2025-08-04 21:03:04 +0930

    place holder top textures

--- src/main/java/com/harleylizard/witches_way/common/WitchesWay.java
+++ src/main/java/com/harleylizard/witches_way/common/WitchesWay.java
@@ -1,6 +1,7 @@
 package com.harleylizard.witches_way.common;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.registry.StrippableBlockRegistry;
 import net.minecraft.resources.ResourceLocation;
 
 public final class WitchesWay implements ModInitializer {
@@ -13,6 +14,15 @@ public final class WitchesWay implements ModInitializer {
 
         WitchesWayBlockEntities.register();
         WitchesWayFeatures.register();
+
+        StrippableBlockRegistry.register(WitchesWayBlocks.ALDER_LOG, WitchesWayBlocks.STRIPPED_ALDER_LOG);
+        StrippableBlockRegistry.register(WitchesWayBlocks.ALDER_WOOD, WitchesWayBlocks.STRIPPED_ALDER_WOOD);
+
+        StrippableBlockRegistry.register(WitchesWayBlocks.HAWTHORN_LOG, WitchesWayBlocks.STRIPPED_HAWTHORN_LOG);
+        StrippableBlockRegistry.register(WitchesWayBlocks.HAWTHORN_WOOD, WitchesWayBlocks.STRIPPED_HAWTHORN_WOOD);
+
+        StrippableBlockRegistry.register(WitchesWayBlocks.ROWAN_LOG, WitchesWayBlocks.STRIPPED_ROWAN_LOG);
+        StrippableBlockRegistry.register(WitchesWayBlocks.ROWAN_WOOD, WitchesWayBlocks.STRIPPED_ROWAN_WOOD);
 
     }
 
