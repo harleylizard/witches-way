7f682049b43ea83257464f65e21623c63890a10d
harleylizard <64570529+harleylizard@users.noreply.github.com>
2025-08-06 13:06:53 +0930

    load block state tags

--- src/main/java/com/harleylizard/witches_way/common/BlockStateTag.java
+++ src/main/java/com/harleylizard/witches_way/common/BlockStateTag.java
@@ -1,21 +1,38 @@
 package com.harleylizard.witches_way.common;
 
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
 import com.mojang.serialization.Codec;
+import com.mojang.serialization.JsonOps;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.core.BlockPos;
+import net.minecraft.server.packs.resources.ResourceManager;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.levelgen.feature.stateproviders.BlockStateProvider;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.io.IOException;
+import java.util.*;
 
 public record BlockStateTag(List<BlockStateProvider> blockStates) {
-    public static final Codec<BlockStateTag> CODEC = RecordCodecBuilder.create(builder -> builder.group(BlockStateProvider.CODEC.listOf().fieldOf("values").forGetter(BlockStateTag::blockStates)).apply(builder, BlockStateTag::new));
+    public static final BlockStateTag EMPTY = new BlockStateTag(List.of());
+
+    public static final Codec<BlockStateTag> CODEC = RecordCodecBuilder.create(builder -> builder.group(BlockStateProvider.CODEC.listOf().fieldOf("values").forGetter(BlockStateTag::blockStates)).apply(builder, list -> list.isEmpty() ? EMPTY : new BlockStateTag(Collections.unmodifiableList(list))));
+
+    private static final Map<String, BlockStateTag> TAGS = new HashMap<>();
 
     public boolean compare(BlockState blockState, RandomSource random, BlockPos blockPos) {
         return blockStates.stream().map(provider -> provider.getState(random, blockPos)).anyMatch(comparing -> compare(comparing, blockState));
+    }
+
+    public BlockStateTag join(BlockStateTag other) {
+        return new BlockStateTag(join(blockStates, other.blockStates));
+    }
+
+    public static <T> List<T> join(List<T> first, List<T> second) {
+        List<T> list = new ArrayList<>(first);
+        list.addAll(second);
+        return Collections.unmodifiableList(list);
     }
 
     public static boolean compare(BlockState comparing, BlockState blockState) {
@@ -30,6 +47,38 @@ public record BlockStateTag(List<BlockStateProvider> blockStates) {
         }
 
         return true;
+    }
+
+    public static void parse(ResourceManager manager) throws IOException {
+        TAGS.clear();
+
+        var gson = new GsonBuilder().create();
+
+        for (var entry : manager.listResources("blockstates", path -> path.toString().endsWith(".json")).entrySet()) {
+            var name = entry.getKey().withPath(path -> path.substring(0, path.indexOf(".json")).substring(path.lastIndexOf("/") + 1)).toString();
+
+            try (var reader = entry.getValue().openAsReader()) {
+                var result = CODEC.parse(JsonOps.INSTANCE, gson.fromJson(reader, JsonElement.class));
+
+                if (result.isError()) {
+                    continue;
+                }
+
+                var tag = result.getOrThrow();
+
+                if (TAGS.containsKey(name)) {
+                    TAGS.put(name, TAGS.get(name).join(tag));
+
+                    continue;
+                }
+
+                TAGS.put(name, result.getOrThrow());
+            }
+        }
+    }
+
+    public static BlockStateTag get(String name) {
+        return TAGS.getOrDefault(name, EMPTY);
     }
 
 }
--- src/main/java/com/harleylizard/witches_way/common/WitchesWay.java
+++ src/main/java/com/harleylizard/witches_way/common/WitchesWay.java
@@ -1,6 +1,7 @@
 package com.harleylizard.witches_way.common;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerWorldEvents;
 import net.fabricmc.fabric.api.loot.v3.LootTableEvents;
 import net.fabricmc.fabric.api.networking.v1.PlayerLookup;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
@@ -12,6 +13,8 @@ import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.storage.loot.LootPool;
 import net.minecraft.world.level.storage.loot.entries.LootItem;
 import net.minecraft.world.level.storage.loot.predicates.LootItemRandomChanceCondition;
+
+import java.io.IOException;
 
 public final class WitchesWay implements ModInitializer {
     public static final String MOD_ID = "witches-way";
@@ -37,6 +40,16 @@ public final class WitchesWay implements ModInitializer {
             if (Blocks.SHORT_GRASS.getLootTable() == resourceKey && lootTableSource.isBuiltin()) {
                 builder.withPool(LootPool.lootPool().with(LootItem.lootTableItem(WitchesWayItems.BELLADONNA_SEEDS).build()).conditionally(LootItemRandomChanceCondition.randomChance(0.75f / 16.0f).build()));
                 builder.withPool(LootPool.lootPool().with(LootItem.lootTableItem(WitchesWayItems.MANDRAKE_SEEDS).build()).conditionally(LootItemRandomChanceCondition.randomChance(0.75f / 16.0f).build()));
+            }
+
+        });
+
+        ServerWorldEvents.LOAD.register((server, level) -> {
+            try {
+                BlockStateTag.parse(server.getResourceManager());
+
+            } catch (IOException e) {
+                throw new RuntimeException(e);
             }
 
         });
